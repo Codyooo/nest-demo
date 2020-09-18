6b3b9419c7ae5e22aa0d43eca9c2b6580ba80a35
hufan <hufan@kuaishou.com>
2020-09-18 11:37:09 +0800

    feat: add ssl support

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,41 @@
  * @author: hufan
  * @Date: 2020-08-31 11:57:19
  * @LastEditors: hufan
- * @LastEditTime: 2020-09-17 16:36:30
+ * @LastEditTime: 2020-09-18 11:36:11
  */
 import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ExpressAdapter } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AllExceptionFilter } from './filters/all-exception.filter';
 import { ResponseInterceptor } from './interceptors/response.interceptor';
+import * as express from 'express';
+import * as http from 'http';
+import * as https from 'https';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const cert = fs.readFileSync(
+    join(`${__dirname}`, '..', 'cert', '1_doubleq.icu_bundle.crt'),
+  );
+  const key = fs.readFileSync(
+    join(`${__dirname}`, '..', 'cert', '2_doubleq.icu.key'),
+  );
+
+  const httpsOptions = {
+    key,
+    cert,
+  };
+
+  const server = express();
+
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+    new ExpressAdapter(server),
+  );
+
   app.setGlobalPrefix('api');
   app.enableCors({});
   app.useStaticAssets(join(__dirname, '..', 'public'));
@@ -39,6 +62,11 @@ async function bootstrap() {
   app.use('/api-docs', (req, res) => res.send(document));
   SwaggerModule.setup('api', app, document);
 
+  await app.init();
+
+  https.createServer(httpsOptions, server).listen(443);
   await app.listen(process.env.PORT || 4000);
+
+  // await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
